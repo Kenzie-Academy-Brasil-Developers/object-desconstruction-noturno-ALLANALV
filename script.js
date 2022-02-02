0f2994b2cf6e7f55f7d1837b7b0c2f81a30dc8a3
ale <ale@ale.com>
2022-02-02 14:48:25 -0300

    feat: feature show result some functions on webPage

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ const testMap = (array) => {
     return result
 };
 
+
 const testFilter = (array) => {
     const result = array.filter(({ id }) => {
         if (id <= 2) {
@@ -104,3 +105,76 @@ const totalsection = (array) => {
 
     return total
 }
+
+const container = document.querySelector('.container');
+const listFunctions = [testMap(itemArray), testFilter(itemArray), testFind(itemArray), filterPrice(itemArray), updatePrice(itemArray)]
+
+function createSection(name = ['testMap', 'testFilter', 'testFind', 'filterPrice', 'updatePrice']) {
+    for (let index = 0; index < listFunctions.length; index++) {
+        const list = document.createElement('ul');
+        const execFunction = document.createElement('h2');
+        execFunction.innerText = ` function ${name[index]}`;
+        execFunction.classList.add('name-function')
+        list.appendChild(execFunction);
+
+        const obj = listFunctions[index];
+        obj.forEach((elem) => {
+            const card = document.createElement('li');
+            const identy = document.createElement('div');
+            const figure = document.createElement('figure');
+            const img = document.createElement('img');
+            const infor = document.createElement('div');
+            const name = document.createElement('h3');
+            const sectionName = document.createElement('span');
+            const descPrice = document.createElement('p');
+            const descStore = document.createElement('p');
+            const spanPrice = document.createElement('span');
+            const spanQuantify = document.createElement('span');
+            const buy = document.createElement('button');
+
+            const { id, product, price, quantity, section } = elem;
+            card.classList.add('cardProduct');
+
+            identy.innerText = id;
+            img.src = './img/carrinho-de-compras.png';
+            name.innerText = product;
+            sectionName.innerText = section;
+            spanPrice.innerText = `R$ ${price.toFixed(2)}`;
+            spanQuantify.innerText = quantity;
+            buy.innerText = 'Comprar';
+
+            identy.classList.add('id');
+            infor.classList.add('info');
+            name.classList.add('titleProduct');
+            sectionName.classList.add('section-product');
+            descPrice.classList.add('description');
+            spanPrice.classList.add('price');
+            spanQuantify.classList.add('quantify');
+            descStore.classList.add('description');
+
+
+            descPrice.innerText = 'Preço: ';
+            descStore.innerText = 'Estoque :';
+            figure.appendChild(img);
+            descPrice.appendChild(spanPrice);
+            descStore.appendChild(spanQuantify);
+
+            infor.appendChild(name);
+            infor.appendChild(sectionName);
+            infor.appendChild(descPrice);
+            infor.appendChild(descStore);
+            infor.appendChild(buy)
+
+            card.appendChild(identy);
+            card.appendChild(figure);
+            card.appendChild(infor);
+
+            list.appendChild(card)
+        });
+
+        container.appendChild(list)
+    }
+}
+
+createSection();
+
